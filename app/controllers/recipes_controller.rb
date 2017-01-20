479f6228a31ce67f76f36a1e1cdde07b17b54e6d
Samantha Morco <samanthamorco@gmail.com>
2017-01-19 19:47:25 -0500

    add form to add a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,14 @@ class RecipesController < ApplicationController
     @recipe = Recipe.first
     render "recipe.html.erb"
   end
+
+  def new_recipe_method
+    render "new_recipe_form.html.erb"
+  end
+
+  def new_recipe_result_method
+    @recipe = Recipe.new(title: params[:input_title], chef: params[:input_chef], ingredients: params[:input_ingredients], directions: params[:input_directions])
+    @recipe.save
+    render "new_recipe_result_view.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/first_recipe", to: "recipes#first_recipe_method"
   # get "/first_recipe" => "recipes#first_recipe_method"
+
+  get "/new_recipe", to: "recipes#new_recipe_method"
+  post "/new_recipe_result", to: "recipes#new_recipe_result_method"
 end
