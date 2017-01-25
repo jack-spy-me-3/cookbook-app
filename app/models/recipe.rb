6542ab52f9c64c5104923e9f40c08f1c100975f3
Samantha Morco <samanthamorco@gmail.com>
2017-01-24 19:49:39 -0500

    model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,23 @@
 class Recipe < ApplicationRecord
+
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_time
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hour(s) " if hours > 0
+    result += "#{minutes} minute(s)" if minutes > 0
+    result
+  end
 end
